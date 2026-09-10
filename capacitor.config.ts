e555ed7e06288aa07764a654aac57610cd5f6d74
Camryn Williams <hammersmodality@gmail.com>
2026-09-10 14:07:37 -0400

    Add Capacitor iOS wrapper

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hammersmodality.app',
+  appName: 'Hammers Modality',
+  webDir: 'dist'
+};
+
+export default config;
